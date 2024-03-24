782602b907f0d38409c0de6a2eb8074a19ef5770
Dante Hart <dante_hart1@baylor.edu>
2024-03-24 14:21:00 -0500

    Implemented Reading CustomerList Customer by Country and sorting by city name. Editing main to create the table for the endpoints

--- apiRest/crud.py
+++ apiRest/crud.py
@@ -1,2 +1,13 @@
 # This will only have read functionality for this assignment
+from sqlalchemy.orm import Session
+
+import models
+import schemas
+
+
+def get_customer(db: Session, cust_id: int):
+    return db.query(models.Customer).filter(models.Customer.customer_id == cust_id).first()
+
+def get_customer_list_by_country(db: Session, country: str):
+    return db.query(models.CustomerList).filter(models.CustomerList.country == country).all().order_by(models.CustomerList.city.desc())
 
