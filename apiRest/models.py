9d98a8e598bbd08acd829b30ecc6e39d7cd493ef
Dante Hart <dante_hart1@baylor.edu>
2024-03-24 14:00:39 -0500

    Created ORMs for tables need to execute SQL statement. Added schemas.py file for Pydantic

---
+++ apiRest/models.py
@@ -0,0 +1,33 @@
+import sqlalchemy.types
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, SmallInteger
+from sqlalchemy.orm import relationship
+
+from db import Base
+
+
+class Customer(Base):
+    __tablename__ = 'customer'
+
+    customer_id = Column(SmallInteger, primary_key=True)
+    store_id = Column(SmallInteger)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+    address_id = Column(SmallInteger)
+    active = Column(SmallInteger)
+    create_date = Column(DateTime)
+    last_update = Column(sqlalchemy.types.TIMESTAMP)
+
+
+class CustomerList(Base):
+    __tablename__ = "customer_list"
+
+    ID = Column(SmallInteger)
+    name = Column(String)
+    address = Column(String)
+    zip_code = Column(String)
+    phone = Column(String)
+    city = Column(String)
+    country = Column(String)
+    notes = Column(String)
+    SID = Column(SmallInteger)
